d062bd51388948314b978da9c72489a55b843d75
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2022-09-11 19:30:49 -0700

    Create fizzbuzz.ps1

---
+++ fizzbuzz.ps1
@@ -0,0 +1,28 @@
+
+for ($i=1; $i -le 35; $i++ ){
+	
+	$output = ""
+	
+	if( $i % 5 -eq 0 ){
+	
+		$output += "Fizz"
+	
+	}
+	
+	if( $i % 3-eq 0){
+		
+		$output += "Buzz"
+		
+	}
+	
+	# Format the number to be single-space padded if less than 10
+	$i_padded = $i
+	
+	if ( $i -lt 10){
+		
+		$i_padded = " $i"
+		
+	}
+
+	Write-Host "$i_padded : $output"
+}
